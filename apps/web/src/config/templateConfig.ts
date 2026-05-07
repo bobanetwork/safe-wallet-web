8be85219f99f07b0ad5790a7cfbfd77742b3d47f
msvstj <savastijonok@gmail.com>
2026-05-07 14:40:18 +0300

    feat(network-config): add boba network configuration

---
+++ apps/web/src/config/templateConfig.ts
@@ -0,0 +1,22 @@
+import type { TemplateConfig } from './constants.extra'
+
+/**
+ * THIS FILE IS AUTO-GENERATED. DO NOT EDIT.
+ * Generated from /networks/boba/config.json
+ */
+const TEMPLATE_CONFIG = {
+  EIP155: false,
+  SUPPORTED_VERSIONS: ['1.3.0', '1.4.1'],
+  SAFE_UTILS_SUPPORTED: false,
+  LOGO_DIMENSIONS: {
+    HEADER: {
+      H: '45px',
+    },
+    WELCOME: {
+      W: '160px',
+      H: '48px',
+    },
+  },
+  IS_LICENSED: false,
+} as TemplateConfig
+export default TEMPLATE_CONFIG
